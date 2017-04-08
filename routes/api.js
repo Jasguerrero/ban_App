54890a91743c81523d833bec80128f4b0fa827e1
Jesus Sanchez Guerrero <jesus.sanchez@spectrumeffect.com>
2017-04-08 11:26:42 -0500

    GET and CREATE posts

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,7 @@
 const
   express = require('express'),
+  mongoose = require('mongoose'),
+  Post = mongoose.model('Post'),
   authMiddleware = require('../utility/authMiddleware');
 
 var router = express.Router();
@@ -9,6 +11,38 @@ router.use(function(req, res, next){
     return authenticated > 0 ? next() : res.status(401).send({message: 'Unauthorized'});
   });
 });
+
+router.route('/posts')
+  .get(function(req, res){
+    Post.find({}, {created_at: 1, text: 1, likes_count: 1, likesArr: 1, comments_count: 1, username: 1}).sort({created_at: -1}).exec(function(err, data){
+      if(err)
+        return res.status(500).send({message: 'OOPS something went wrong'});
+
+      for(let d of data){
+        if(d.likesArr.indexOf(req.deco.userID) != -1){
+          d.liked = true;
+				}
+        d.likesArr = undefined;
+      }
+      return res.send(data);
+    });
+  })
+  .post(function(req, res){
+    if(!req.body.text){
+      return res.status(422).send({message: 'Missing params'})
+    }
+    newPost = new Post();
+    newPost.text = req.body.text;
+    newPost.userID = req.deco.userID;
+    newPost.username = req.deco.name;
+
+    newPost.save(function(err, new_post){
+      if(err){
+        return res.status(422).send({message: 'Validation failed'});
+      }
+      return res.status(201).send({message: 'Post created'});
+    });
+  });
 
 
 module.exports = router
