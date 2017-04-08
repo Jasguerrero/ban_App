9d75a915bf2ff0450ec463e495d3c633278c7531
Jesus Sanchez Guerrero <jesus.sanchez@spectrumeffect.com>
2017-04-08 17:49:40 -0500

    Login functionality frontend

---
+++ public/js/login.js
@@ -0,0 +1,21 @@
+function login(){
+  var email = document.getElementById('login_email').value.toString();
+  var password = document.getElementById('login_password').value.toString();
+
+  var json = {"email": email, "password": password};
+  $.ajax({
+    dataType: 'json',
+    url: '/api/v1/auth/login',
+    contentTypeString: 'application/x-www-form-urlencoded',
+    type: 'POST',
+    data: json,
+    success: function(data, status){
+      document.cookie = 'token' + '=' + data.token
+      location.reload();
+    },
+    error: function(){
+      //TODO
+      alert("Error")
+    }
+  });
+}
--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(){
-  getPosts();
+  getPosts(readToken());
 });
 
-function getPosts(){
-  //TODO get token from cookies
-  token = 'token'
+function getPosts(token){
   $.ajax({
     dataType: 'json',
     url: '/api/v1/posts?token=' + token,
@@ -14,7 +12,7 @@ function getPosts(){
       initPosts(data);
     },
     error: function(){
-      showLogin();
+      renderAuth();
     }
   });
 }
@@ -28,7 +26,27 @@ function initPosts(data){
   }
 }
 
-function showLogin(){
+function renderAuth(){
   var app = document.getElementById('app');
-  app.innerHTML = '<div>LOGIN</div>'
+  app.innerHTML =
+  '<div class="row">'+
+    '<div class="col-md-8 col-md-offset-2">'+
+      '<form class="form-vertical" role="form">'+
+        '<input type="text" placeholder="email" class="form-control" id="login_email"/>'+
+        '<input type="password" placeholder="password" class="form-control" id="login_password"/>'+
+        '<button type="button" class="btn btn-info form-control" onclick="login()">Login</button>'+
+      '</form>'+
+    '<div>'+
+  '</div>';
+}
+
+function readToken(){
+  var nameEQ = 'token' + "=";
+  var co = document.cookie.split(';');
+  for(var i=0;i < co.length;i++) {
+      var c = co[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
 }
